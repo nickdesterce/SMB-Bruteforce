1a5cd97e4b0462f3d2c1f02011b348d82e3150bd
nickdesterce <andresdevaux@hotmail.com>
2025-08-18 16:05:32 +1000

    main.bat

--- main.bat
+++ main.bat
@@ -1,10 +1,29 @@
 @echo off
-title SMB Bruteforce
-echo.
-set /p ip="Enter ID: "
-set /p user="Enter Username: "
-set /p wordlist="Enter Password List: "
+title Roblox Bruteforce
 
+echo /$$$$$$$                        /$$                     /$$$$$$$$                                     
+echo ! $$__  $$                      ! $$                    ! $$_____/                                     
+echo ! $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$       ! $$     /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ 
+echo ! $$$$$$$  /$$__  $$! $$  ! $$!_  $$_/   /$$__  $$      ! $$$$$ /$$__  $$ /$$__  $$ /$$_____/ /$$__  $$
+echo ! $$__  $$! $$  \__/! $$  ! $$  ! $$    ! $$$$$$$$      ! $$__/! $$  \ $$! $$  \__/! $$      ! $$$$$$$$
+echo ! $$  \ $$! $$      ! $$  ! $$  ! $$ /$$! $$_____/      ! $$   ! $$  ! $$! $$      ! $$      ! $$_____/
+echo ! $$$$$$$/! $$      !  $$$$$$/  !  $$$$/!  $$$$$$$      ! $$   !  $$$$$$/! $$      !  $$$$$$$!  $$$$$$$
+echo !_______/ !__/       \______/    \___/   \_______/      !__/    \______/ !__/       \_______/ \_______/
+                                                                                                       
+echo.
+                                              echo MADE BY: @solar.xe
+echo [16:00] - [STARTUP] - 5
+echo [16:00] - [STARTUP] - 4
+echo [16:00] - [STARTUP] - 3
+echo [16:00] - [STARTUP] - 2
+echo [16:00] - [STARTUP] - 1
+echo 
+echo [16:00] - Insert key : "
+echo [16:00] - Whitelisted.
+set /p [1] Checker
+set /p [2] Bruteforce
+echo
+[?] Choose an option: "
 set /a count=1
 for /f %%a in (%wordlist%) do (
   set pass=%%a
@@ -27,3 +46,4 @@ echo [ATTEMPT %count%] [%pass%]
 set /a count=%count%+1
 
 if %errorlevel% EQU 0 goto success
+
