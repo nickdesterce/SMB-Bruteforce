67cc48df61e55cbb509ea1729c389e1f8b8a9faa
nickdesterce <andresdevaux@hotmail.com>
2025-08-25 14:55:54 +1000

    Update main.bat

--- main.bat
+++ main.bat
@@ -35,7 +35,7 @@ ping localhost -n 1 >nul
 echo         [Checker][0m  [95m[2][0m
 ping localhost -n 1 >nul
 echo         [2FA ByPass][0m   [95m[3][0m
-echo|set /p=".          [90;1m╚══>[0m"
+
 choice /c 123 >nul
 if /I "%errorlevel%" EQU "1" (
   start bruteforce.bat
@@ -55,6 +55,7 @@ if /I "%errorlevel%" EQU "3" (
   goto :start
 
 )
+
 
 
 
