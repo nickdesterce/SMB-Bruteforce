d077b3fa7490a3c5e2dd8d1ef401a0f91dd87935
nickdesterce <andresdevaux@hotmail.com>
2025-08-27 16:24:53 +1000

    Update main.bat

--- main.bat
+++ main.bat
@@ -30,14 +30,15 @@ echo [16:00] - Whitelisted.
 ping localhost -n 3 >nul
 
 ping localhost -n 1 >nul
-echo         [95m [Bruteforce] [0m
+echo  [90;1m#═╦═══════»[0m  [92m[Checker][0m [95m[1][0m
 ping localhost -n 1 >nul
-echo        [95m [Checker] [0m
+echo  [90;1m╚═╦══════»[0m  [92m[Bruteforce][0m  [95m[2][0m
 
 choice /c 123 >nul
 if /I "%errorlevel%" EQU "1" (
   start bruteforce.bat
   goto :start
+
 
 
 
