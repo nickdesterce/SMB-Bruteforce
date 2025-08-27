7e95a3ef220d3e5e862cc7f94137d421522f068a
nickdesterce <andresdevaux@hotmail.com>
2025-08-27 16:22:57 +1000

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,66 +1,44 @@
 
 @echo off
-title Roblox Bruteforce
+title py.exe
 
-
- echo  /$$$$$$$                        /$$                     /$$$$$$$$                                     
- echo  ! $$__  $$                      ! $$                    ! $$_____/                                     
- echo  ! $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$       ! $$     /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ 
- echo  ! $$$$$$$  /$$__  $$! $$  ! $$!_  $$_/   /$$__  $$      ! $$$$$ /$$__  $$ /$$__  $$ /$$_____/ /$$__  $$
- echo  ! $$__  $$! $$  \__/! $$  ! $$  ! $$    ! $$$$$$$$      ! $$__/! $$  \ $$! $$  \__/! $$      ! $$$$$$$$
- echo  ! $$  \ $$! $$      ! $$  ! $$  ! $$ /$$! $$_____/      ! $$   ! $$  ! $$! $$      ! $$      ! $$_____/
- echo  ! $$$$$$$/! $$      !  $$$$$$/  !  $$$$/!  $$$$$$$      ! $$   !  $$$$$$/! $$      !  $$$$$$$!  $$$$$$$
-  echo !_______/ !__/       \______/    \___/   \_______/      !__/    \______/ !__/       \_______/ \_______/
+ echo /$$$$$$$                        /$$                     /$$$$$$$$                                     
+ echo ! $$__  $$                      ! $$                    ! $$_____/                                     
+ echo ! $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$       ! $$     /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ 
+ echo ! $$$$$$$  /$$__  $$! $$  ! $$!_  $$_/   /$$__  $$      ! $$$$$ /$$__  $$ /$$__  $$ /$$_____/ /$$__  $$
+ echo ! $$__  $$! $$  \__/! $$  ! $$  ! $$    ! $$$$$$$$      ! $$__/! $$  \ $$! $$  \__/! $$      ! $$$$$$$$
+ echo ! $$  \ $$! $$      ! $$  ! $$  ! $$ /$$! $$_____/      ! $$   ! $$  ! $$! $$      ! $$      ! $$_____/
+ echo ! $$$$$$$/! $$      !  $$$$$$/  !  $$$$/!  $$$$$$$      ! $$   !  $$$$$$/! $$      !  $$$$$$$!  $$$$$$$
+ echo !_______/ !__/       \______/    \___/   \_______/      !__/    \______/ !__/       \_______/ \_______/
                                                                                                        
 echo.
                                               echo MADE BY: @solar.xe
 echo [16:00] - [STARTUP] - 5
-pause
-echo.
+ping localhost -n 2 >nul
 echo [16:00] - [STARTUP] - 4
-pause
-echo.
+ping localhost -n 2 >nul
 echo [16:00] - [STARTUP] - 3
-pause
-echo.
+ping localhost -n 2 >nul
 echo [16:00] - [STARTUP] - 2
-pause
-echo.
+ping localhost -n 2 >nul
 echo [16:00] - [STARTUP] - 1
-pause
-echo.
-set /p ip="Insert key IP: "
+ping localhost -n 1 >nul
 
+set /p ip="Insert key: "
 echo [16:00] - Whitelisted.
 
-pause
+ping localhost -n 3 >nul
 
 ping localhost -n 1 >nul
-echo         [Bruteforce][0m [95m[1][0m
+echo         [95m [Bruteforce] [0m
 ping localhost -n 1 >nul
-echo         [Checker][0m  [95m[2][0m
-ping localhost -n 1 >nul
-echo         [2FA ByPass][0m   [95m[3][0m
+echo        [95m [Checker] [0m
 
 choice /c 123 >nul
 if /I "%errorlevel%" EQU "1" (
   start bruteforce.bat
   goto :start
-)
-if /I "%errorlevel%" EQU "2" (
-  start main.vbs
-  goto :start
-)
 
-
-
-
-if /I "%errorlevel%" EQU "3" (
-  cd..
-  start files
-  goto :start
-
-)
 
 
 
